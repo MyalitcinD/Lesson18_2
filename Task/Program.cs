6c2d706d378c94ce6abab7792856f5ed3e1d1d2f
Денис <mden1996.07@gmail.com>
2024-03-14 22:13:09 +0300

    done

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,48 @@ namespace Task
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Введите строку скобок");
+            string str = Console.ReadLine();
+
+            Check(str);
+            Console.ReadKey();
         }
+        static bool Check(string str)
+        {
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> dict = new Dictionary<char, char>();
+            dict.Add('{', '}');
+            dict.Add('[', ']');
+            dict.Add('(', ')');
+            dict.Add('<', '>');
+
+            dict.Add('}', '0');
+            dict.Add(']', '0');
+            dict.Add(')', '0');
+            dict.Add('>', '0');
+
+            foreach(char ch in str) {
+
+                if(dict[ch] != '0') {
+                    stack.Push(dict[ch]);
+                }
+                else if((stack.Count != 0) && (ch == stack.Peek())) {
+                    stack.Pop();
+                }
+                else { stack.Push('0'); break; }
+            }
+
+            if(stack.Count == 0) {
+                Console.WriteLine("Последовательность корректна");
+                return true;
+            }
+            else {
+                Console.WriteLine("Последовательность некорректна");
+                return false;
+            }
+
+        }
+
     }
 }
